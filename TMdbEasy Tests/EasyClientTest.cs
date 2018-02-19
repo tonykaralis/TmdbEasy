e66ab499df0cbda0a9d8635f137bf4a15d1eca00
Tony <karalistony@hotmail.com>
2018-02-19 02:08:27 +0000

    adds valid api key in test

--- TMdbEasy Tests/EasyClientTest.cs
+++ TMdbEasy Tests/EasyClientTest.cs
@@ -11,24 +11,24 @@ namespace TMdbEasy_Tests
     class EasyClientTest
     {
         [TestFixture]
-        [Category ("Initialization")]
+        [Category("Initialization")]
         public class InitializeTest
         {
             [TestCase(null)]
             [TestCase("")]
             [TestCase(" ")]
             public void EmptyApiKey_ReturnsArgumentException(string _apikey)
-            {    
+            {
                 //Assert
-                Assert.Throws<ArgumentException>(()=> new TMdbEasy.EasyClient(_apikey));
+                Assert.Throws<ArgumentException>(() => new SUT.EasyClient(_apikey));
             }
 
-            [TestCase("123124")]            
+            [TestCase("6d4b546936310f017557b2fb498b370b")]
             public void SecuredParam_AssignsSecureUrl(string _apikey, bool secure = true)
             {
                 //Arrange
                 var obj = new SUT.EasyClient(_apikey, secure);
-                                
+
                 //Assert
                 Assert.AreEqual(obj.Secured, true);
                 Assert.AreEqual(obj.Url, "https://api.themoviedb.org/");
@@ -43,7 +43,7 @@ namespace TMdbEasy_Tests
                 //Assert
                 Assert.AreEqual(obj.Secured, false);
                 Assert.AreEqual(obj.Url, "http://api.themoviedb.org/");
-            }
+          }
         }
     }
 }
