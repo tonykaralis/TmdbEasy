5894818c81574aec5ee8ee9dac2154c1d9a95d37
Tony <karalistony@hotmail.com>
2018-02-19 02:10:57 +0000

    add a temporary movie class

---
+++ TMdbEasy Tests/ClientBaseTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SUT = TMdbEasy;
+
+namespace TMdbEasy_Tests
+{
+    class ClientBaseTests
+    {
+        [TestFixture]
+        [Category("Http methods")]
+        public class DeserializeJsonTest
+        {
+            [TestCase("https://api.themoviedb.org/3/search/movie/?api_key=6d4b546936310f017557b2fb498b370b&query=007")]
+            public async Task CorrectQuery_DoesNotReturnNull(string query)
+            {
+                //arrange
+                var obj = new SUT.EasyClient("6d4b546936310f017557b2fb498b370b");
+                //act
+                SUT.Movie mov = await obj.GetMovieInfoAsync(query);
+                //assert
+                Assert.IsNotNull(mov);
+            }
+        }
+    }
+}
---
+++ TMdbEasy/TmdbObjects/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMdbEasy
+{
+    public class MovieSearchResult
+    {
+        public int Vote_count { get; set; }
+        public int Id { get; set; }
+        public bool Video { get; set; }
+        public double Vote_average { get; set; }
+        public string Title { get; set; }
+        public double Popularity { get; set; }
+        public string Poster_path { get; set; }
+        public string Original_language { get; set; }
+        public string Original_title { get; set; }
+        public List<int> Genre_ids { get; set; }
+        public string Backdrop_path { get; set; }
+        public bool Adult { get; set; }
+        public string Overview { get; set; }
+        public string Release_date { get; set; }
+    }
+
+    public class Movie
+    {
+        public int Page { get; set; }
+        public int Total_results { get; set; }
+        public int Total_pages { get; set; }
+        public List<MovieSearchResult> Results { get; set; }
+    }
+}
