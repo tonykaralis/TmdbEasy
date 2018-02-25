79c9dc3aacfce862fe06f3c4bbcc4bf87e3cbda3
Tony <karalistony@hotmail.com>
2018-02-25 18:33:19 +0000

    Adds Tmdb Error objects

---
+++ TMdbEasy/Errors/Error401.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMdbEasy.Errors
+{
+    public class Error401
+    {
+        public string Status_message { get; set; }
+        public bool Success { get; set; }
+        public int Status_code { get; set; }
+    }
+}
---
+++ TMdbEasy/Errors/Error404.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMdbEasy.Errors
+{
+    public class Error404
+    {
+        public int Status_code { get; set; }
+        public string Status_message { get; set; }
+    }
+}
