36af898a6d56e9efe37aa5de47d8cf5496ab578a
Tony <karalistony@hotmail.com>
2018-02-25 18:33:43 +0000

    Adds collectionApi interface and object

---
+++ TMdbEasy/ApiObjects/CollectionApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMdbEasy.ApiInterfaces;
+
+namespace TMdbEasy.ApiObjects
+{
+    internal class CollectionApi : ICollectionApi
+    {
+        public CollectionApi() { }
+    }
+}
