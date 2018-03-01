91113a6b9976a689f0578ad2dc244bc08105fd49
Tony <karalistony@hotmail.com>
2018-03-01 16:08:02 +0000

    Adds companies and company details

---
+++ TMdbEasy/TmdbObjects/Companies/CompanyDetailscs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMdbEasy.TmdbObjects.Companies
+{
+    public class CompanyDetails
+    {
+        public object Description { get; set; }
+        public string Headquarters { get; set; }
+        public string Homepage { get; set; }
+        public int Id { get; set; }
+        public object Logo_path { get; set; }
+        public string Name { get; set; }
+        public object Parent_company { get; set; }
+    }
+}
