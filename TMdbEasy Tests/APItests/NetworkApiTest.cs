dfa8301d2783ff25d4c93268e6be7c2c8218eda4
Tony <karalistony@hotmail.com>
2018-03-06 21:56:59 +0000

    Adds INetworksApi, implementation and test

---
+++ TMdbEasy Tests/APItests/NetworkApiTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMdbEasy.ApiInterfaces;
+using SUT = TMdbEasy;
+
+namespace TMdbEasy_Tests.APItests
+{
+    [Category("NetworkApi")]
+    internal static class NetworkApiTest
+    {
+        [TestFixture]
+        [Category("NetworkApi")]
+        public class GetDetailsAsync
+        {
+            [TestCase(296096321)]
+            public void IncorrectId_ThrowsException(int id)
+            {
+                //arrange
+                var obj = new SUT.EasyClient("6d4b546936310f017557b2fb498b370b");
+                var d = obj.GetApi<INetworksApi>().Value;
+                //assert
+                Assert.Throws<AggregateException>(() => { SUT.TmdbObjects.Networks nt = d.GetDetailsAsync(id).Result; });
+            }
+        }
+    }
+}
---
+++ TMdbEasy/ApiInterfaces/INetworksApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMdbEasy.TmdbObjects;
+
+namespace TMdbEasy.ApiInterfaces
+{
+    public interface INetworksApi : IBase
+    {
+        /// <summary>
+        /// Get the details of a network.
+        /// </summary>
+        /// <param name="id">Network Id, typically taken from a previous api call</param>       
+        /// <returns></returns>
+        Task<Networks> GetDetailsAsync(int id);
+    }
+}
