a6ca34f35b974f70f8b6710376298f78c84a0938
Tony <karalistony@hotmail.com>
2018-05-07 14:30:14 +0100

    Adds some assembly details

--- TMdbEasy/Properties/AssemblyInfo.cs
+++ TMdbEasy/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TMdbEasy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# Wrapper library for TMDB")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tony Karalis")]
 [assembly: AssemblyProduct("TMdbEasy")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
