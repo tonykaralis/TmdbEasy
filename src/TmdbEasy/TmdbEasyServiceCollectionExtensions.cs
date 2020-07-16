90f5edfb710ed07d2328a3c0389fd1d7af4fed4b
T.Karalis <t.karalis@mmr-research.com>
2020-07-16 23:32:50 +0100

    WIP:Moves some files around, adds new .net standard and core projects

---
+++ src/TmdbEasy/TmdbEasyServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TmdbEasy
+{
+    public static class TmdbEasyServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTmdbEasyClient(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection;
+        }
+    }
+}
