3ad3921173ff3f35c7f53cd98c70a5430f671d1e
T.Karalis <t.karalis@mmr-research.com>
2020-07-18 12:10:03 +0100

    WIP add unit tests for v3 and v4 url and ssl options

---
+++ Tests/V3TmdbEasyClientTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using TmdbEasy.Configurations;
+using TmdbEasy.Interfaces;
+
+namespace TmdbEasy.Tests
+{
+    [TestFixture]
+    public class V3TmdbEasyClientTests
+    {
+        private readonly IJsonDeserializer _jsonDeserializer;
+
+        public V3TmdbEasyClientTests()
+        {
+            _jsonDeserializer = new NewtonSoftDeserializer();
+        }
+
+        [Test]
+        public void OptionsWithSsl_Off_SetsCorrectVersionOfUrl_WithNoSsl()
+        {
+            var options = new TmdbEasyOptions()
+            {
+                UseSsl = false
+            };
+
+            var client = new TmdbEasyClientv3(_jsonDeserializer, options);
+
+            Assert.AreEqual(Constants.TmdbUrl3, client.GetBaseUrl());
+        }
+
+        [Test]
+        public void OptionsWithSsl_On_SetsCorrectVersionOfUrl_WithSsl()
+        {
+            var options = new TmdbEasyOptions()
+            {
+                UseSsl = true
+            };
+
+            var client = new TmdbEasyClientv3(_jsonDeserializer, options);
+
+            Assert.AreEqual(Constants.TmdbUrl3Ssl, client.GetBaseUrl());
+        }
+    }
+}
