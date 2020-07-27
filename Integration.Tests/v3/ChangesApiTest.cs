850f7649f26bc5939d582e9c2f792e1b535df1f5
T.Karalis <t.karalis@mmr-research.com>
2020-07-27 09:46:46 +0100

    Adds additional changesApi test

--- Integration.Tests/v3/ChangesApiTest.cs
+++ Integration.Tests/v3/ChangesApiTest.cs
@@ -4,6 +4,7 @@ using TmdbEasy.Apis;
 using TmdbEasy.Data.Changes;
 using TmdbEasy.Data.Reviews;
 using TmdbEasy.DTO;
+using TmdbEasy.Enums;
 using TmdbEasy.Integration.Tests.TestFixtures;
 using TmdbEasy.Interfaces;
 
@@ -27,6 +28,29 @@ namespace TMdbEasy.Integration.Tests.v3
                 UserApiKey = userApiKey,
                 Start_date = "25/07/2020",
                 End_date = "26/07/2020"
+            };
+
+            ChangeList changeList = await serviceUnderTest.GetChangeListAsync(request);
+
+            Assert.IsNotNull(changeList);
+            Assert.IsNotEmpty(changeList.Results);
+        }
+
+        [Test]
+        public async Task GetChangeListAsync_SpecificType_ReturnsChangeList()
+        {
+            ITmdbEasyClient client = GetTestV3Client();
+
+            string userApiKey = GetApiKey();
+
+            IChangesApi serviceUnderTest = new ChangesApi(client);
+
+            var request = new ChangeListRequest()
+            {
+                UserApiKey = userApiKey,
+                Start_date = "25/07/2020",
+                End_date = "26/07/2020",
+                Type = ChangeType.TV
             };
 
             ChangeList changeList = await serviceUnderTest.GetChangeListAsync(request);
