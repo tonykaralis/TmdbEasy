c6c98d1ebdede4722dadea8e20cc62d47ca759bd
T.Karalis <t.karalis@mmr-research.com>
2020-07-27 14:49:55 +0100

    Adds collection api implementation and tests

---
+++ TmdbEasy/Apis/CollectionApi.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using TmdbEasy.Constants;
+using TmdbEasy.DTO;
+using TmdbEasy.DTO.Images;
+using TmdbEasy.DTO.Other;
+using TmdbEasy.Interfaces;
+
+namespace TmdbEasy.Apis
+{
+    public class CollectionApi : BaseApi, ICollectionApi
+    {
+        public CollectionApi(ITmdbEasyClient client) : base(client) { }
+
+        public async Task<Collections> GetDetailsAsync(IdRequest request, string language = "en")
+        {
+            string queryString = $"collection/{request.Id}?{QueryConstants.ApiKeyVariable}{GetApiKey(request.UserApiKey)}&language={language}";
+
+            return await _client.GetResponseAsync<Collections>(queryString).ConfigureAwait(false);
+        }
+
+        public async Task<Images> GetImagesAsync(IdRequest request, string language = "en")
+        {
+            string queryString = $"collection/{request.Id}/images?{QueryConstants.ApiKeyVariable}{GetApiKey(request.UserApiKey)}&language={language}";
+
+            return await _client.GetResponseAsync<Images>(queryString).ConfigureAwait(false);
+        }
+    }
+}
--- TmdbEasy/DTO/BaseRequest.cs
+++ TmdbEasy/DTO/BaseRequest.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// Optional parameter. You would only use this parameter if you were providing a user specific api key
         /// on each request. If this is not the case, and you are using a single apikey for the entire app, 
-        /// then please set apikey globally via the TmdbEasyOptions configuration
+        /// then please set apikey globally via the TmdbEasyOptions configuration.
         /// </summary>
         public string UserApiKey { get; set; }
     }
