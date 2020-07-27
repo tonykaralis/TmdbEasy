06744079edec56b00416c1a320c5160f4fb1211f
T.Karalis <t.karalis@mmr-research.com>
2020-07-27 09:43:31 +0100

    Condenses params into single class, adds test for changes api

---
+++ TmdbEasy/DTO/ChangeListRequest.cs
@@ -0,0 +1,13 @@
+﻿using TmdbEasy.Enums;
+
+namespace TmdbEasy.DTO
+{
+    public class ChangeListRequest
+    {
+        public string UserApiKey { get; set; }
+        public string End_date { get; set; }
+        public string Start_date { get; set; }
+        public int Page { get; set; } = 1;
+        public ChangeType Type { get; set; } = ChangeType.Movie;
+    }
+}
