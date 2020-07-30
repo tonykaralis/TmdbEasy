d2ee249e99363138c040c67e3e5aed212a1a34e9
T.Karalis <t.karalis@mmr-research.com>
2020-07-30 12:48:36 +0100

    WIP add movie api tests

--- tests/Integration/MovieApiTest.cs
+++ tests/Integration/MovieApiTest.cs
@@ -1,65 +1,67 @@
-﻿//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using TMdbEasy.ApiInterfaces;
-//using SUT = TMdbEasy;
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+using TmdbEasy.Apis;
+using TmdbEasy.DTO.Images;
+using TmdbEasy.DTO.Movies;
+using TmdbEasy.Interfaces;
+using TmdbEasy.Tests.Integration.TestFixtures;
 
-//namespace TMdbEasy_Tests.APItests
-//{
-//    internal static class MovieApiTest
-//    {
-//        [TestFixture]
-//        [Category("MovieApi")]
-//        public class GetDetailsAsync
-//        {
-//            [TestCase(5018857)]
-//            public void IncorrectId_ThrowsException(int id, string language = "en")
-//            {
-//                var d = Constants.SecureTestClient.GetApi<IMovieApi>().Value;
+namespace TmdbEasy.Tests.Integration
+{
+    [TestFixture]
+    [Category("MovieApi")]
+    internal class MovieApiTest : TestBaseForV3
+    {
+        [TestCase(505)]
+        public async Task GetDetailsAsync_ValidId_CustomApiKey_ReturnsValidResult(int id)
+        {
+            var _requestHandler = new RequestHandler(_clientWithNoApiKey);
 
-//                Assert.Throws<AggregateException>(() => { SUT.TmdbObjects.Movies.MovieFullDetails mov = d.GetDetailsAsync(id).Result; });
-//            }
-//        }
+            IMovieApi apiUnderTest = new MovieApi(_requestHandler);
 
-//        [TestFixture]
-//        [Category("MovieApi")]
-//        public class GetImagesAsync
-//        {
-//            [TestCase(296096321)]
-//            public void IncorrectId_ThrowsException(int id, string language = "en")
-//            {
-//                var d = Constants.SecureTestClient.GetApi<IMovieApi>().Value;
+            MovieFullDetails result = await apiUnderTest.GetDetailsAsync(id, apiKey: _userApiKey);
 
-//                Assert.Throws<AggregateException>(() => { SUT.TmdbObjects.Images.Images ima = d.GetImagesAsync(id).Result; });
-//            }
-//        }
+            Assert.IsNotNull(result);
+            Assert.AreEqual(id, result.Id);
+        }
 
-//        [TestFixture]
-//        [Category("MovieApi")]
-//        public class GetAlternativetitlesAsync
-//        {
-//            [TestCase(296096321)]
-//            public void IncorrectId_ThrowsException(int id, string language = "en")
-//            {
-//                var d = Constants.SecureTestClient.GetApi<IMovieApi>().Value;
+        [TestCase(550)]
+        public async Task GetImagesAsync_IncorrectId_ThrowsException(int id)
+        {
+            var _requestHandler = new RequestHandler(_clientWithNoApiKey);
 
-//                Assert.Throws<AggregateException>(() => { SUT.TmdbObjects.Movies.AlternativeTitle tit = d.GetAlternativeTitlesAsync(id).Result; });
-//            }
-//        }
+            IMovieApi apiUnderTest = new MovieApi(_requestHandler);
 
-//        [TestFixture]
-//        [Category("MovieApi")]
-//        public class SearchByActorAsync
-//        {
-//            [TestCase("Brad Pitt")]
-//            public void FamousActor_ReturnResults(string actorName)
-//            {
-//                var d = Constants.SecureTestClient.GetApi<IMovieApi>().Value;
+            Images result = await apiUnderTest.GetImagesAsync(id, _userApiKey);
 
-//                CollectionAssert.IsNotEmpty(d.SearchByActorAsync(actorName).Result.Results);
-//            }
-//        }
-//    }
-//}
+            Assert.IsNotNull(result);
+            Assert.AreEqual(id, result.Id);
+        }
+
+        [TestCase(550)]
+        public async Task GetAlternativetitlesAsync_IncorrectId_ThrowsException(int id)
+        {
+            var _requestHandler = new RequestHandler(_clientWithNoApiKey);
+
+            IMovieApi apiUnderTest = new MovieApi(_requestHandler);
+
+            AlternativeTitle result = await apiUnderTest.GetAlternativeTitlesAsync(id, apiKey: _userApiKey);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(id, result.Id);
+        }
+
+        [TestCase("Brad Pitt")]
+        public async Task SearchByActorAsync_FamousActor_ReturnResults(string actorName)
+        {
+            var _requestHandler = new RequestHandler(_clientWithNoApiKey);
+
+            IMovieApi apiUnderTest = new MovieApi(_requestHandler);
+
+            MovieList result = await apiUnderTest.SearchByActorAsync(actorName, apiKey: _userApiKey);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotEmpty(result.Results);
+        }
+    }
+}
